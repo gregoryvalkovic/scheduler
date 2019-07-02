31246d0705e67b80d5a78cdad2e867fa756f0a40
Greg <gregoryvalkovic@gmail.com>
2019-07-02 20:41:17 +1000

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :users do
       resources :events
   end
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
